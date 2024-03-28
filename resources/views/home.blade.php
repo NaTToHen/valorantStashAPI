817b77376647e7078b1133d8a7dfb11d9b8e60ad
Henrique Bonatto <nattohen@gmail.com>
2024-03-28 10:34:32 -0300

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ValorantStash-API</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <h1>API - <a href="https://valorantstash.info">valorantstash.info</a></h1>
+    </body>
+</html>
